722841ac490dfe046e05d6f724feaba1a592005d
shivi-choudhary <34636947+shivichoudhary@users.noreply.github.com>
2020-09-21 19:47:03 +0530

    let's Create whatsapp clone using React.Js and Firebase
    This is react js and firebase project.
    in this project i am creating a Whatsapp-web-clone using React.js and Google's Firebase database and google authentication. this is a real time chat application.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBYOW5iDZYPJ1rm6JNM1JQfy9aZ-PL_Hc0",
+    authDomain: "whatsappp-clone-9627b.firebaseapp.com",
+    databaseURL: "https://whatsappp-clone-9627b.firebaseio.com",
+    projectId: "whatsappp-clone-9627b",
+    storageBucket: "whatsappp-clone-9627b.appspot.com",
+    messagingSenderId: "85987801869",
+    appId: "1:85987801869:web:8ee4f11ebb7b56bcaf8e44",
+    measurementId: "G-18E8PK9SX8"
+  };
